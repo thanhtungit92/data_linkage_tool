83cb6f06b7722020a1e6a878510114b4bb826849
Tung PT <tungpt@puppetteam.com>
2016-10-19 16:14:38 +0700

    change setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='data_linkage_tool',
-      version='0.3',
+      version='0.4',
       description='Do python project',
       url='https://github.com/thanhtungit92/data_linkage_tool.git',
       author='Tung PT',
