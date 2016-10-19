8a8e913db6d16e240579059a2e2ad81d7b786666
Tung PT <tungpt@puppetteam.com>
2016-10-19 16:06:20 +0700

    first step

---
+++ data_linkage_tool/__init__.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def helloProject():
+	print pd.date_range('20130101', periods=6)
+	print "Hello python"
--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+from setuptools import setup
 
+setup(name='data_linkage_tool',
+      version='0.3',
+      description='Do python project',
+      url='https://github.com/thanhtungit92/data_linkage_tool.git',
+      author='Tung PT',
+      author_email='thanhtungit92@gmail.com',
+      license='MIT',
+      packages=['data_linkage_tool'],
+      install_requires=[
+          'pandas',
+      ],
+      zip_safe=False
+)
