4a9ead975af9df269bb97930722ceee14bb89bf9
Tung PT <tungpt@puppetteam.com>
2016-10-20 16:49:17 +0700

    add lib for setup, and unit test file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='data_linkage_tool',
-      version='0.4',
+      version='0.5',
       description='Do python project',
       url='https://github.com/thanhtungit92/data_linkage_tool.git',
       author='Tung PT',
@@ -9,7 +9,8 @@ setup(name='data_linkage_tool',
       license='MIT',
       packages=['data_linkage_tool'],
       install_requires=[
-          'pandas',
+            'pandas',
+            'logging',
       ],
       zip_safe=False
 )
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,9 @@
 import unittest
+import data_linkage_tool
+
+class TestInitMethods(unittest.TestCase):
+    def test(self):
+        self.assertEqual(data_linkage_tool.doForUnitTest(), 100)
+
+if __name__ == '__main__':
+    unittest.main()
