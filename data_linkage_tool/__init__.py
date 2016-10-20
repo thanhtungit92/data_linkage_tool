6105df6577ee4cf3759aa3255f44e479a9f4ceb2
Tung PT <tungpt@puppetteam.com>
2016-10-20 16:48:02 +0700

    add function for unit test

--- data_linkage_tool/__init__.py
+++ data_linkage_tool/__init__.py
@@ -1,5 +1,11 @@
 import pandas as pd
+import logging
 
 def helloProject():
 	print pd.date_range('20130101', periods=6)
 	print "Hello python"
+
+
+def doForUnitTest():
+	logging.warning('Do a simple function for unittest')
+	return 100
